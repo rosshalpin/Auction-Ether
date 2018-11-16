815b61532684e08b6b2c08b885e4a2130768ef1c
Ross Halpin <rosshalpin20@gmail.com>
2018-11-16 12:31:51 +0000

    Cleaning up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import firebase from 'firebase';
-
 import Web3 from 'web3';
-
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -14,7 +11,6 @@ import Typography from '@material-ui/core/Typography';
 import { AddCircle } from '@material-ui/icons';
 //const message = require('./deploy');
 const fs = require('fs');
-
 
 function AuctionCard() {
     let cardClassName = "Auction-card";
@@ -50,54 +46,62 @@ function AuctionCard() {
     );
 };
 
-
-const web3 = new Web3(window.web3.currentProvider);
+const web3 = new Web3(window.ethereum);
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        var config = {
-            apiKey: "AIzaSyAPB2SsaCcDPXUKL3_zslSMf7S0mW2zxH0",
-            authDomain: "auctionether.firebaseapp.com",
-            databaseURL: "https://auctionether.firebaseio.com",
-            projectId: "auctionether",
-            storageBucket: "auctionether.appspot.com",
-            messagingSenderId: "285598191042"
-        };
-        firebase.initializeApp(config);
-		this.state = {
-		  cardArray: []
-		}
-    }
+	
+	state = {
+		cardArray: [],
+	};
+	
+	async componentDidMount() {
+		
+	}
 	
 	payForAuction = async () => {
 		if (typeof web3 !== 'undefined') {
+			await window.ethereum.enable();
+			
 			const accounts = await web3.eth.getAccounts();
 			console.log(accounts[0])
 			
-			this.addNewCard();
+			web3.eth.sendTransaction({
+				from: accounts[0],
+				to: '0x7657bC53995C2B55104E389a06d28f1Bf1237AA0',
+				//value: '1000000000000000'
+				value: web3.utils.toWei('0.0001', 'ether')
+			})
+			.on('transactionHash', function(hash){
+				
+			})
+			.on('receipt', function(receipt){
+				
+			})
+			.on('confirmation', function(confirmationNumber, receipt){
+				console.log(confirmationNumber, receipt)
+				this.addNewCard();
+			})
+			.on('error', console.error); // If a out of gas error, the second parameter is the receipt.
+			
+			web3.eth.getTransaction('0x70bc6d1483bc85db08be633c4d3a6e7e94354f5546033ed91e17e1eb485a6083')
+			.then(console.log);
 			
 		}else {
 			console.log('You must use metamask to interact with this website')
 		}
 		
-		
-		
 	};
 	
 	addNewCard = () => {
-		this.setState( (state) => {
-			var nCard = new AuctionCard();
-			this.state.cardArray = this.state.cardArray.concat([nCard]);
-			return state;
-		});
-		//console.log(this.state.cardArray)
+		var nCard = new AuctionCard();
+		let newArray = this.state.cardArray.concat([nCard]);
+		this.setState({cardArray: newArray});
 	};
 	
 	cardAdder  = () => { 
 		let cardClassName = "Auction-card";
 		return(
-			<Card className={cardClassName} onClick={() => {this.payForAuction()}} >
+			<Card className={cardClassName} onClick={() => {this.addNewCard()}} >
 				<CardActionArea style={{height: '280px'}} >
 						<AddCircle style={{fontSize: '64px', color: '#bcbcbc'}} ></AddCircle>
 				</CardActionArea>
