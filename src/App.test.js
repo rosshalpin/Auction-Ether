84dacd4ddd0fa1f3b0791a0bf9d91b41a44b4f46
Ross Halpin <rosshalpin20@gmail.com>
2019-03-11 22:53:42 +0000

    Initial testing commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+import {shallow, mount} from 'enzyme';
+
+
+const wrapper = mount(<App />);
+
+
+it('loads the data', async() => {
+  var v = await wrapper.instance().handleExchangeRate();
+  expect(v).toEqual(5);
+
 });
+
+
+
