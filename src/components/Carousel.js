2eec4aa655e34d0db14976e8670de87d99927715
Ross Halpin <rosshalpin20@gmail.com>
2019-03-16 23:10:46 +0000

    clean up

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,29 +7,6 @@ import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import {MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-const tutorialSteps = [
-  {
-    imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
-  },
-  {
-    imgPath:
-      'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-];
-
 const white_theme = createMuiTheme({
 	palette: {
 		primary: {
@@ -92,7 +69,7 @@ class TextMobileStepper extends React.Component {
         <img
           className={classes.img}
           src={this.state.images[activeStep]}
-          alt={tutorialSteps[activeStep].label}
+          alt={[activeStep]}
         />
         <MuiThemeProvider theme={white_theme}>
         <MobileStepper
